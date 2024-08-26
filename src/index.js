f2c9208e741c4efd975681cb3c3d6e1a61d6a75f
Tusharzade2002 <tusharzade29@gmail.com>
2024-08-26 21:19:43 +0530

    files created

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
-
+import { createBrowserRouter ,RouterProvider } from 'react-router-dom';
+import Home from './Views/Home/Home'
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
 
-   <h1>Reacct</h1>
+const router = createBrowserRouter([
+   {
+      path:'/',
+      element:<Home />
+   }
+])
 
-);
+root.render(<RouterProvider router={router}  />)
 
 
