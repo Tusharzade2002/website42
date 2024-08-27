c5a043b0bb1426b457d2843d5db88d20b8573e7b
Tusharzade2002 <tusharzade29@gmail.com>
2024-08-28 00:33:13 +0530

    images adding

--- src/Component/Foodcard/Foodcard.js
+++ src/Component/Foodcard/Foodcard.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import './Foodcard.css'
-function Foodcard({title ,description}) {
+import  veg from './icons8-veg-48.png'
+import  nonveg  from './icons8-non-veg-48.png'
 
-  
+function Foodcard({imgurl,title ,description,isveg}) { 
   return (
     <div className='Foodcard'>
+      <img 
+      src={imgurl}
+      className='food-card-image'
+      alt='food'
+      />
       <h1 className='card-title'>{title}</h1>
       <h2 className='card-description'>{description}</h2>
+      <img className='card-type' src={ isveg ? veg : nonveg} alt="icon"/>
     </div>
   )
 }
--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -6,12 +6,17 @@ function Home() {
     <div className='main-container'>
        <h1 className='Brand-name'>Gabbar Da Dhaba</h1>
       <Foodcard 
+      imgurl="https://img.freepik.com/premium-photo/juicy
+      -aloo-paratha-cutlets-popular-street-food-aloo-parat
+      ha-alu-paratha-picture_636537-377633.jpg?w=740"
       title="Alu paratha"
       description="Delicious Alu paratha with fresh ingredients"
+      isveg={true}
       />
        <Foodcard 
       title="biryani"
       description="Delicious Biryani paratha with fresh ingredients"
+      isveg={false}
       />
     </div>
   )
