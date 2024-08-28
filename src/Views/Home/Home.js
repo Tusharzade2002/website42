3c1a52d5b3a8d80b96867f2cb72df13872b4952f
Tusharzade2002 <tusharzade29@gmail.com>
2024-08-28 21:56:16 +0530

    panner tikka & chiken curry added

--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -18,7 +18,20 @@ function Home() {
        imgurl="https://img.freepik.com/premium-photo/savory-biryani
        -with-succulent-chicken-rustic-tabledelectable-indian-cuisine_960396-874102.jpg?w=740"
       title="biryani"
-      description="Delicious Biryani paratha with fresh ingredients"
+      description="Delicious Biryani  with fresh ingredients"
+      isveg={false}
+      />
+      <Foodcard 
+       imgurl="https://img.freepik.com/premium-photo/chicken-curry-pan-with-lemongrass_1174497-78536.jpg?w=740"
+      title="Chiken curry"
+      description="Delicious Chiken curry with fresh ingredients"
+      isveg={false}
+      />
+      <Foodcard 
+       imgurl="https://img.freepik.com/free-photo/chicken-skewers-with-slices-apples-chili_2829-19992.jpg?t
+       =st=1724862230~exp=1724865830~hmac=3a1c8ea1b8efb3f7f1713e9af11634c98198dd029ea50edb6ba11a412e17bdd0&w=740"
+      title="Panner Tikka"
+      description="Delicious Panner Tikka with fresh ingredients"
       isveg={false}
       />
       </div>
