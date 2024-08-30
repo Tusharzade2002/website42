c76ee644a24e53a0e575e6f49a13310187f5fea4
Tusharzade2002 <tusharzade29@gmail.com>
2024-08-30 19:24:14 +0530

    configratiin added

--- src/Component/Foodcard/Foodcard.js
+++ src/Component/Foodcard/Foodcard.js
@@ -2,23 +2,24 @@ import React from 'react'
 import './Foodcard.css'
 import  veg from './icons8-veg-48.png'
 import  nonveg  from './icons8-non-veg-48.png'
+import { THEME } from '../../Config/data'
 
 function Foodcard({imgurl,title ,description,isveg,price}) { 
   return (
-    <div className='Foodcard'>
+    <div className='Foodcard' >
       <img 
       src={imgurl}
       className='food-card-image'
       alt='food'
       />
-      <h1 className='card-title'>{title}</h1>
+      <h1 className='card-title' style={{color:THEME.highlight}}>{title}</h1>
       <h2 className='card-description'>{description}</h2>
       <img className='card-type' src={ isveg ? veg : nonveg} alt="icon"/>
 
      {
       price ? <p className='food-card-price'> ₹ {price}</p> :<p className='food-not-avialble'> Not Available</p>
      }
-     <button className='Buy-btn'>Add To Card</button>
+     <button className='Buy-btn' style={{backgroundColor:THEME.button}}>Add To Card</button>
     </div>
   )
 }
